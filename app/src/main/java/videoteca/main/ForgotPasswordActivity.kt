a7926e454f1160925655696096b9b02891de135a
alen bicanic <alen.bicanic01@gmail.com>
2024-06-15 15:51:15 +0200

    aggiunto il reset della password

--- app/src/main/java/videoteca/main/ForgotPasswordActivity.kt
+++ app/src/main/java/videoteca/main/ForgotPasswordActivity.kt
@@ -1,14 +1,67 @@
 package videoteca.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import videoteca.main.api.AuthService
 
+/**
+ * Activity per la gestione del recupero della password.
+ */
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var btn_recover:Button
+    private lateinit var btn_back:Button
+    private lateinit var et_email: EditText
+
+    /**
+     * Metodo chiamato alla creazione dell'activity.
+     *
+     * @param savedInstanceState Stato precedente dell'istanza, se disponibile.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_forgot_password2)
 
+        btn_recover = findViewById(R.id.btn_recover_pws)
+        btn_back = findViewById(R.id.btn_back)
+        et_email = findViewById(R.id.et_email_forgot)
+
+        btn_recover.setOnClickListener{
+            val email:String = et_email.text.toString().trim()
+            if(email.isEmpty()){
+                Toast.makeText(this,
+                    getString(R.string.no_email_has_been_entered), Toast.LENGTH_SHORT).show()
+            }else{
+                resetPassword(email)
+            }
+        }
+
     }
+
+    /**
+     * Avvia il processo di reset della password utilizzando il servizio di autenticazione.
+     *
+     * @param email L'indirizzo email per il quale eseguire il reset della password.
+     */
+    private fun resetPassword(email:String) {
+        AuthService.resetPassword(this, email){ success, message ->
+            if(success){
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/videoteca/main/api/AuthService.kt
+++ app/src/main/java/videoteca/main/api/AuthService.kt
@@ -1,8 +1,10 @@
 package videoteca.main.api
 
+import android.content.Context
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import videoteca.main.R
 
 /**
  * Oggetto che gestisce l'autenticazione degli utenti utilizzando Firebase Authentication.
@@ -72,6 +74,35 @@ object AuthService {
      */
     fun logout() {
         mAuth.signOut()
+    }
+
+    /**
+     * Invia una richiesta di reset della password all'indirizzo email specificato.
+     *
+     * @param context Il contesto corrente, utilizzato per ottenere le risorse stringa.
+     * @param email L'indirizzo email dell'utente che ha dimenticato la password.
+     * @param callback Una funzione di callback che viene chiamata al termine dell'operazione.
+     *                 Il primo parametro indica se l'operazione è stata completata con successo (true) o meno (false).
+     *                 Il secondo parametro contiene un messaggio descrittivo del risultato.
+     */
+    fun resetPassword(context: Context, email: String, callback: (Boolean, String) -> Unit) {
+        val mAuth = FirebaseAuth.getInstance()
+
+        mAuth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val msg:String =
+                        context.getString(R.string.send_password_reset_e_mail_send_with_success)
+                    Log.i(TAG, msg)
+                    callback(true, msg)
+                } else {
+                    val msg:String =
+                        context.getString(R.string.failed_to_send_password_reset_e_mail)
+                    Log.e(TAG, msg)
+                    val errorMessage = task.exception?.message ?: msg
+                    callback(false, errorMessage)
+                }
+            }
     }
 
 
